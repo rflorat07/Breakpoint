99233cb17bfaa72d57d493b902c1366adbcbf6b8
Roger Florat <rflorat@cidiverte.it>
2018-01-24 17:14:27 +0100

    Creating InsetTextField and ShadowView Subclasses

---
+++ Breakpoint/Breakpoint/View/InsetTextField.swift
@@ -0,0 +1,21 @@
+//
+//  InsetTextField.swift
+//  Breakpoint
+//
+//  Created by Roger Florat on 24/01/18.
+//  Copyright © 2018 Roger Florat. All rights reserved.
+//
+
+import UIKit
+
+class InsetTextField: UITextField {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Breakpoint/Breakpoint/View/ShadowView.swift
@@ -0,0 +1,21 @@
+//
+//  ShadowView.swift
+//  Breakpoint
+//
+//  Created by Roger Florat on 24/01/18.
+//  Copyright © 2018 Roger Florat. All rights reserved.
+//
+
+import UIKit
+
+class ShadowView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
