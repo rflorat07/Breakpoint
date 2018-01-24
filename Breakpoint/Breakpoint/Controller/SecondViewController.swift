478e3eda62a4c89d22ebb1ebb323e9b455667bbe
Roger Florat <rflorat@cidiverte.it>
2018-01-24 15:33:55 +0100

    Setting Up DataService / Creating Firebase Database Users

---
+++ Breakpoint/Breakpoint/Controller/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Breakpoint
+//
+//  Created by Roger Florat on 24/01/18.
+//  Copyright © 2018 Roger Florat. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,9 @@
+# APP - BREAKPOINT ( IOS 11 - Swift 4 )
+
+## Installation Instructions
+
+COCOAPODS - https://cocoapods.org/
+
+1. pod 'Firebase/Core' (4.3.0)
+2. pod 'Firebase/Database' (4.1.0)
+3. pod 'Firebase/Auth' (4.2.1)
