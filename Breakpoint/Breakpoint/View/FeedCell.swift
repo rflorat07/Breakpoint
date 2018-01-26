7ab1d9286f1a3b36e23c7b7fdb33d7101cbcb262
Roger Florat <rflorat@cidiverte.it>
2018-01-26 10:48:54 +0100

    Building FeedCell

---
+++ Breakpoint/Breakpoint/View/FeedCell.swift
@@ -0,0 +1,23 @@
+//
+//  FeedCell.swift
+//  Breakpoint
+//
+//  Created by Roger Florat on 26/01/18.
+//  Copyright © 2018 Roger Florat. All rights reserved.
+//
+
+import UIKit
+
+class FeedCell: UITableViewCell {
+
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var emailLbl: UILabel!
+    @IBOutlet weak var contentLbl: UILabel!
+    
+    func configurateCell(profileImage: UIImage, email: String, content: String) {
+        self.profileImage.image = profileImage
+        self.emailLbl.text = email
+        self.contentLbl.text = content
+    }
+    
+}
