e3819082b8e2332051e5b5f2e1ad1de9aefff8ab
Roger Florat <rflorat@cidiverte.it>
2018-01-29 16:07:41 +0100

    Creating Group Model and Getting All Groups from Firebase

--- Breakpoint/Breakpoint/Controller/GroupsVC.swift
+++ Breakpoint/Breakpoint/Controller/GroupsVC.swift
@@ -12,16 +12,24 @@ class GroupsVC: UIViewController {
     
     @IBOutlet weak var groupsTableView: UITableView!
     
-
+    var groupsArray = [Group]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         groupsTableView.delegate = self
         groupsTableView.dataSource = self
     }
 
-
-
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        DataService.instance.REF_GROUPS.observe(.value) { (snapshor) in
+            DataService.instance.getAllGroups { (returnGroupsArray) in
+                self.groupsArray = returnGroupsArray
+                self.groupsTableView.reloadData()
+            }
+        }
+    }
 }
 
 
@@ -32,13 +40,14 @@ extension GroupsVC: UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return groupsArray.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = groupsTableView.dequeueReusableCell(withIdentifier: "groupCell", for: indexPath) as? GroupCell else { return UITableViewCell() }
+        let group = groupsArray[indexPath.row]
         
-        cell.configureCell(title: "Nerd Herd", description: "The nerdiest nerd around.", memberCount: 3)
+        cell.configureCell(title: group.groupTitle, description: group.groupDesc, memberCount: group.memberCount)
         
         return cell
         
